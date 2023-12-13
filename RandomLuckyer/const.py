1e803acba0ebc702b34099b04e77d1e2cea22b79
MoYeRanQianZhi <108128735+MoYeRanqianzhi@users.noreply.github.com>
2023-12-14 01:40:55 +0800

    Create const.py

---
+++ RandomLuckyer/const.py
@@ -0,0 +1,20 @@
+configureFile = 'luckyer.json'
+defaultConfigureContent = {
+    "students": list(range(1, 51)),
+    "window": {
+        "alpha": 0.8,
+        "topmost": True,
+        "infoArea": {
+            'font': '微软雅黑',
+            'fontSize': 12
+        },
+        "luckyerArea": {
+            'font': '微软雅黑',
+            'fontSize': 100
+        },
+        "buttonArea": {
+            'font': '微软雅黑',
+            'fontSize': 15
+        }
+    }
+}
